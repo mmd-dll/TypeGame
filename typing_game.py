be6a4eaf65c458dbcf3b77223abc8f6bf923a40b
mohammad reza <id.suzuya@gmail.com>
2025-05-01 21:36:47 +0330

    Update typing_game.py

--- typing_game.py
+++ typing_game.py
@@ -1,8 +1,7 @@
-from time import time
+from time import time, sleep
 from os import system, get_terminal_size
-from colorama import Fore, Style, init
+from colorama import Fore, Style, init,Back
 from lorem import sentence
-from time import sleep
 import os
 import msvcrt
 
@@ -27,9 +26,9 @@ def StartMenu():
 
     cols = get_terminal_size().columns
 
-    for text in name.splitlines():
-        padding = (cols - len(text)) // 2
-        print(Fore.CYAN + " " * max(padding, 0) + text)
+    for line in name.splitlines():
+        padding = (cols - len(line)) // 2
+        print(Fore.CYAN + " " * max(padding, 0) + line)
         sleep(0.05)
 
     padding = (cols - len(or_name)) // 2
@@ -37,23 +36,24 @@ def StartMenu():
 
     print(Fore.LIGHTRED_EX + "Telegram:", Fore.LIGHTWHITE_EX + "https://t.me/RedSnows")
     print(Fore.LIGHTRED_EX + "GitHub:", Fore.LIGHTWHITE_EX + "https://github.com/mmd-dll")
-    print('')
-    print(Fore.BLUE +'[1]' + Fore.WHITE,'Start game')
-    print(Fore.BLUE +'[2]' + Fore.WHITE,'Exit')
+    print()
+    print(Fore.BLUE + '[1]' + Fore.WHITE, 'Start game')
+    print(Fore.BLUE + '[2]' + Fore.WHITE, 'Exit')
+
     while True:
         user_Choice = input('\n\nEnter option number: ')
         try:
             user_Choice = int(user_Choice)
         except:
-            print("Enter an number not Str!")
-        
+            print("Enter a number, not a string!")
+            continue
+
         if user_Choice == 1:
             break
         elif user_Choice == 2:
             exit()
 
 def typing_game():
-
     StartMenu()
     clear_screen()
 
@@ -63,7 +63,7 @@ def typing_game():
     sleep(1)
     clear_screen()
     print(Fore.WHITE + Style.BRIGHT + text + "\n")
-    
+
     user_input = ""
     index = 0
     start_time = time()
@@ -72,18 +72,22 @@ def typing_game():
         char = msvcrt.getwch()
 
         if char == text[index]:
-            user_input += char
-            index += 1
+            user_input += Fore.GREEN + char
         else:
-            pass
+            user_input += Fore.RED + text[index]
+
+        index += 1
 
         clear_screen()
-        print(Fore.WHITE + Style.BRIGHT + text + "\n")
-        
-        colored_input = (Fore.GREEN + user_input +
-                         Fore.RESET + text[index:])
-        
-        clear_screen()
+
+        remaining_text = text[index:]
+        if remaining_text:
+            next_char = Back.MAGENTA + Fore.BLACK + remaining_text[0] + Fore.RESET + Back.RESET
+            rest_chars = remaining_text[1:]
+            colored_input = user_input + next_char + rest_chars
+        else:
+            colored_input = user_input
+
         print(colored_input)
 
     end_time = time()
